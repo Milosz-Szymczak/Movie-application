d6fde28fa4896e5fd6273e593e06df17290d12d8
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-03-19 19:13:46 +0100

    Added findByRole method

--- src/main/java/pl/milosz/moviedatabase/security/repository/UserRepository.java
+++ src/main/java/pl/milosz/moviedatabase/security/repository/UserRepository.java
@@ -1,9 +1,13 @@
-package pl.milosz.moviedatabase.repository;
+package pl.milosz.moviedatabase.security.repository;
 
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.milosz.moviedatabase.entity.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends ListCrudRepository<User, Long> {
+
+    Optional<User> findByRole(User.Role role);
 }
