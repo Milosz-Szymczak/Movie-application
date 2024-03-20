fb188162a7be6ba4412053a42713f318dc50d1a7
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-03-20 14:49:12 +0100

    Added test for findByRole Query

---
+++ src/test/java/pl/milosz/moviedatabase/security/repository/UserRepositoryTest.java
@@ -0,0 +1,39 @@
+package pl.milosz.moviedatabase.security.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import pl.milosz.moviedatabase.entity.User;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User admin;
+
+    @BeforeEach
+    public void init() {
+        admin = User.builder().userId(1L).username("admin").password("admin").role(User.Role.ADMIN).build();
+    }
+
+    @Test
+    void findByRole_should_ReturnUserByRole() {
+        userRepository.save(admin);
+
+        Optional<User> findUserByRole = userRepository.findByRole(admin.getRole());
+
+        Assertions.assertThat(findUserByRole).isNotNull();
+        Assertions.assertThat(findUserByRole.get().getRole()).isEqualTo(admin.getRole());
+    }
+}
