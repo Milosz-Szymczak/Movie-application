25968ecca345ff8282933d3747f174f8023a71b3
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-03-21 12:15:58 +0100

    Added UserNotFoundException

---
+++ src/main/java/pl/milosz/moviedatabase/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package pl.milosz.moviedatabase.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
