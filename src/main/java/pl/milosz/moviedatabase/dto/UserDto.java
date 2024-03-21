54779f2a094fb55f63309d3e9645bee136018de6
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-03-21 12:09:53 +0100

    Change role variable to String

--- src/main/java/pl/milosz/moviedatabase/dto/UserDto.java
+++ src/main/java/pl/milosz/moviedatabase/dto/UserDto.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import pl.milosz.moviedatabase.entity.User;
 
 @Data
 @AllArgsConstructor
@@ -14,5 +13,5 @@ public class UserDto {
     private Long userId;
     private String username;
     private String password;
-    private User.Role role;
+    private String role;
 }
