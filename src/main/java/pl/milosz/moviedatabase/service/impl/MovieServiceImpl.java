f76bd0c7eb3d8a48deafa1114869dfbe25c0c216
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-03-21 12:08:41 +0100

    Added saveMovie method

--- src/main/java/pl/milosz/moviedatabase/service/impl/MovieServiceImpl.java
+++ src/main/java/pl/milosz/moviedatabase/service/impl/MovieServiceImpl.java
@@ -1,8 +1,14 @@
 package pl.milosz.moviedatabase.service.impl;
 
 import org.springframework.stereotype.Service;
+import pl.milosz.moviedatabase.dto.MovieDto;
+import pl.milosz.moviedatabase.entity.Movie;
+import pl.milosz.moviedatabase.mapper.MovieMapper;
 import pl.milosz.moviedatabase.repository.MovieRepository;
 import pl.milosz.moviedatabase.service.MovieService;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class MovieServiceImpl implements MovieService {
@@ -11,4 +17,11 @@ public class MovieServiceImpl implements MovieService {
     public MovieServiceImpl(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
     }
+
+    @Override
+    public Movie saveMovie(MovieDto movieDto) {
+        Movie movie = MovieMapper.toEntity(movieDto);
+        return movieRepository.save(movie);
+    }
+
 }
