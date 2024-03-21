6d67712345df282b37d7df57fdfa8706c93386b3
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-03-21 12:14:44 +0100

    Added new UserMovieRelation entity

---
+++ src/main/java/pl/milosz/moviedatabase/entity/UserMovieRelation.java
@@ -0,0 +1,29 @@
+package pl.milosz.moviedatabase.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "user_movie_relations")
+public class UserMovieRelation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userMovieRelationId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+
+}
