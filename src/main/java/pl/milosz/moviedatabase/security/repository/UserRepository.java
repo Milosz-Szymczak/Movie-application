72bdb6ee221569f26254454b23c9acf22965a80b
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-03-21 12:17:10 +0100

    Added findByUserName method

--- src/main/java/pl/milosz/moviedatabase/security/repository/UserRepository.java
+++ src/main/java/pl/milosz/moviedatabase/security/repository/UserRepository.java
@@ -9,5 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends ListCrudRepository<User, Long> {
 
+    Optional<User> findByUsername(String username);
     Optional<User> findByRole(User.Role role);
 }
