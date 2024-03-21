b6e83eec4391cf1fe6fe2183623ad66e5f9e0277
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-03-21 12:08:02 +0100

    Added saveMovie method

--- src/main/java/pl/milosz/moviedatabase/service/MovieService.java
+++ src/main/java/pl/milosz/moviedatabase/service/MovieService.java
@@ -1,4 +1,10 @@
 package pl.milosz.moviedatabase.service;
 
+import pl.milosz.moviedatabase.dto.MovieDto;
+import pl.milosz.moviedatabase.entity.Movie;
+
+import java.util.Optional;
+
 public interface MovieService {
+    Movie saveMovie(MovieDto movieDto);
 }
