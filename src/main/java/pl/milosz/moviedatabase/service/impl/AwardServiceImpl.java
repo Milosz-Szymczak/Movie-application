8ffe80c99017af532d186104aebfb1d51cdf56e2
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-03-22 16:02:20 +0100

    Added getAwards, saveAward methods impl

--- src/main/java/pl/milosz/moviedatabase/service/impl/AwardServiceImpl.java
+++ src/main/java/pl/milosz/moviedatabase/service/impl/AwardServiceImpl.java
@@ -1,8 +1,14 @@
 package pl.milosz.moviedatabase.service.impl;
 
 import org.springframework.stereotype.Service;
+import pl.milosz.moviedatabase.dto.AwardDto;
+import pl.milosz.moviedatabase.entity.Award;
+import pl.milosz.moviedatabase.entity.Movie;
+import pl.milosz.moviedatabase.mapper.AwardMapper;
 import pl.milosz.moviedatabase.repository.AwardRepository;
 import pl.milosz.moviedatabase.service.AwardService;
+
+import java.util.List;
 
 @Service
 public class AwardServiceImpl implements AwardService {
@@ -11,5 +17,20 @@ public class AwardServiceImpl implements AwardService {
 
     public AwardServiceImpl(AwardRepository awardRepository) {
         this.awardRepository = awardRepository;
+    }
+
+    @Override
+    public List<AwardDto> getAwards(Movie movie) {
+        List<Award> allAward = awardRepository.findAll();
+        return allAward.stream()
+                .filter(c -> c.getMovie().getMovieId().equals(movie.getMovieId()))
+                .map(AwardMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public void saveAward(AwardDto awardDto) {
+        Award award = AwardMapper.toEntity(awardDto);
+        awardRepository.save(award);
     }
 }
