8aace952da99e657c033d859d0db6f3fcc7e362e
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-03-23 12:52:47 +0100

    Added methods for rating service

--- src/main/java/pl/milosz/moviedatabase/service/RatingService.java
+++ src/main/java/pl/milosz/moviedatabase/service/RatingService.java
@@ -1,4 +1,12 @@
 package pl.milosz.moviedatabase.service;
 
+import pl.milosz.moviedatabase.dto.UserDto;
+import pl.milosz.moviedatabase.entity.Movie;
+
 public interface RatingService {
+    void saveRating(Movie movieById, int rating, UserDto loggedUser);
+
+    String getOverallRatingForMovieById(Long movieId);
+
+    boolean hasUserRatedMovie(Long movieId, UserDto loggedUser);
 }
