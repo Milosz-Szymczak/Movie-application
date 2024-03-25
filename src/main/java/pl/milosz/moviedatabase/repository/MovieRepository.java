2fbe21fd28e990d17cf4a5e3d23dd55a21f07478
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-03-25 19:14:08 +0100

    Added Query findByTitleContainingIgnoreCase

--- src/main/java/pl/milosz/moviedatabase/repository/MovieRepository.java
+++ src/main/java/pl/milosz/moviedatabase/repository/MovieRepository.java
@@ -1,9 +1,16 @@
 package pl.milosz.moviedatabase.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.milosz.moviedatabase.entity.Movie;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends ListCrudRepository<Movie, Long> {
+
+    @Query("SELECT m FROM Movie m WHERE LOWER(m.title) LIKE LOWER(concat('%', :keyword, '%'))")
+    List<Movie> findByTitleContainingIgnoreCase(@Param("keyword") String keyword);
 }
