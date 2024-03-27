2df9be54edc5a16ef041d9abb58e0063879508cf
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-03-27 21:34:35 +0100

    Added tests of delete, update methods

--- src/test/java/pl/milosz/moviedatabase/service/impl/AwardServiceImplTest.java
+++ src/test/java/pl/milosz/moviedatabase/service/impl/AwardServiceImplTest.java
@@ -9,13 +9,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import pl.milosz.moviedatabase.dto.AwardDto;
 import pl.milosz.moviedatabase.entity.Award;
 import pl.milosz.moviedatabase.entity.Movie;
+import pl.milosz.moviedatabase.exception.AwardNotFoundException;
 import pl.milosz.moviedatabase.repository.AwardRepository;
 
 import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AwardServiceImplTest {
@@ -56,5 +58,48 @@ class AwardServiceImplTest {
 
         verify(awardRepository).save(any(Award.class));
 
+    }
+
+    @Test
+    void deleteAward_should_deleteAward() {
+        // Given
+        Long awardId = 1L;
+        Award award = new Award();
+        when(awardRepository.findById(awardId)).thenReturn(Optional.of(award));
+
+        // When
+        awardService.deleteAward(awardId);
+
+        // Then
+        verify(awardRepository, times(1)).findById(awardId);
+        verify(awardRepository, times(1)).delete(award);
+    }
+
+    @Test
+    void updateAward_should_updateAward() {
+        // Given
+        Long awardId = 1L;
+        String updatedAwardName = "Updated Award Name";
+        Award award = new Award();
+        when(awardRepository.findById(awardId)).thenReturn(Optional.of(award));
+
+        // When
+        awardService.updateAward(awardId, updatedAwardName);
+
+        // Then
+        verify(awardRepository, times(1)).findById(awardId);
+        verify(awardRepository, times(1)).save(award);
+    }
+
+    @Test
+    void updateAward_should_ThrowException_WhenNotFoundAward() {
+        // Given
+        Long awardId = 1L;
+        String updatedAwardName = "Updated Award Name";
+        when(awardRepository.findById(awardId)).thenReturn(Optional.empty());
+        // When, Then
+        assertThrows(AwardNotFoundException.class, () -> awardService.updateAward(awardId, updatedAwardName));
+        verify(awardRepository, times(1)).findById(awardId);
+        verify(awardRepository, never()).save(any());
     }
 }
