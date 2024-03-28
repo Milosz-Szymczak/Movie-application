b0041b288a481e2d9347c512823b2e80ab428287
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-03-28 17:36:37 +0100

    Added getMoviesAddedByUser method

--- src/main/java/pl/milosz/moviedatabase/service/UserMovieRelationService.java
+++ src/main/java/pl/milosz/moviedatabase/service/UserMovieRelationService.java
@@ -1,8 +1,14 @@
 package pl.milosz.moviedatabase.service;
 
+import pl.milosz.moviedatabase.dto.MovieDto;
 import pl.milosz.moviedatabase.dto.UserDto;
 import pl.milosz.moviedatabase.entity.Movie;
+import pl.milosz.moviedatabase.entity.UserMovieRelation;
+
+import java.util.List;
 
 public interface UserMovieRelationService {
     void saveRelation(UserDto loggedUser, Movie movie);
+
+    List<MovieDto> getMoviesAddedByUser(UserDto loggedUser);
 }
--- src/main/java/pl/milosz/moviedatabase/service/impl/UserMovieRelationImpl.java
+++ src/main/java/pl/milosz/moviedatabase/service/impl/UserMovieRelationImpl.java
@@ -1,13 +1,17 @@
 package pl.milosz.moviedatabase.service.impl;
 
 import org.springframework.stereotype.Service;
+import pl.milosz.moviedatabase.dto.MovieDto;
 import pl.milosz.moviedatabase.dto.UserDto;
 import pl.milosz.moviedatabase.entity.Movie;
 import pl.milosz.moviedatabase.entity.User;
 import pl.milosz.moviedatabase.entity.UserMovieRelation;
+import pl.milosz.moviedatabase.mapper.MovieMapper;
 import pl.milosz.moviedatabase.mapper.UserMapper;
 import pl.milosz.moviedatabase.repository.UserMovieRelationRepository;
 import pl.milosz.moviedatabase.service.UserMovieRelationService;
+
+import java.util.List;
 
 @Service
 public class UserMovieRelationImpl implements UserMovieRelationService {
@@ -27,5 +31,14 @@ public class UserMovieRelationImpl implements UserMovieRelationService {
                 .movie(movie)
                 .build();
         userMovieRelationRepository.save(build);
+    }
+
+    @Override
+    public List<MovieDto> getMoviesAddedByUser(UserDto loggedUser) {
+        return userMovieRelationRepository.findAll().stream()
+                .filter(user -> user.getUser().getUserId().equals(loggedUser.getUserId()))
+                .map(UserMovieRelation::getMovie)
+                .map(MovieMapper::toDto)
+                .toList();
     }
 }
